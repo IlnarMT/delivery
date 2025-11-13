dc24c4f48a5df5e9e9ba8a8c1df219dfb7a7dc1f
Мухаметзанов Ильнар Тальгатович <i.mukhametzanov@cn-innov.ru>
2025-11-13 21:45:04 +0300

    init

---
+++ src/test/java/tm/ilnar/delivery/DeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package tm.ilnar.delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
